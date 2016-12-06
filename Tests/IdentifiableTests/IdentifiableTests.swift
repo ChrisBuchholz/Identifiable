14c99b5a37cf1d2bff45400bfbe9a6d27a1094d5
Christoffer Buchholz <chris@chrisbuchholz.me>
2016-12-07 00:22:02 +0100

    added dumb tests and travis config

--- Tests/IdentifiableTests/IdentifiableTests.swift
+++ Tests/IdentifiableTests/IdentifiableTests.swift
@@ -5,7 +5,7 @@ class IdentifiableTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(Identifiable().text, "Hello, World!")
+        XCTAssertEqual(1, 1)
     }
 
 
